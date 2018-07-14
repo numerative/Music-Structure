235f7121e95c342bb64ed3ef8537246100c1a95e
numerative <michael.hathi@gmail.com>
2018-07-14 13:21:12 +0530

    Add new activity for Player List     Create New Layout for ListView

---
+++ app/src/main/java/com/michaelhat/musicstructureplayer/SongListActivity.java
@@ -0,0 +1,13 @@
+package com.michaelhat.musicstructureplayer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SongListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_song_list);
+    }
+}
