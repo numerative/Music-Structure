913361a2ba68a68084c05ef5969d789b586a602a
numerative <michael.hathi@gmail.com>
2018-07-17 19:00:33 +0530

    Selecting a song from the list now shows up in the player

--- app/src/main/java/com/michaelhat/musicstructureplayer/MainActivity.java
+++ app/src/main/java/com/michaelhat/musicstructureplayer/MainActivity.java
@@ -3,17 +3,40 @@ package com.michaelhat.musicstructureplayer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.player_screen_song1)
+    TextView playedSongName;
+    @BindView(R.id.player_screen_album)
+    TextView playedAlbumName;
+    @BindView(R.id.player_screen_artist)
+    TextView playedArtistName;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Intent getIntent = getIntent();
+        try {
+            //Try setting the text. If intent in null, NullPointer will caught.
+            playedSongName.setText(getIntent.getStringExtra("songName"));
+            playedAlbumName.setText(getIntent.getStringExtra("albumName"));
+            playedArtistName.setText(getIntent.getStringExtra("artistName"));
+        } catch (NullPointerException e) {
+            Log.e("Bundle is empty", e.toString());
+        }
 
+        //Open SongsListActivity
         ImageView playlistIcon = findViewById(R.id.playlist_icon);
         playlistIcon.setOnClickListener(new View.OnClickListener() {
             @Override
